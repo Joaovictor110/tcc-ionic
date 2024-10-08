68a173981f0d28fb84dc6b02733e4cc071f2e529
Joaovictor110 <joaovictoroliveira089@gnail.com>
2024-10-08 14:17:15 -0300

    atualização

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  solicitacoes = [
+    {
+      id: 1,
+      nome: 'Solicitação 1',
+      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnuwTPl2ZShJOyiI30x2P-YILtHrxCwlFUHWzA5JCPltSVsrfrzR1kVIvTaa5hRoO7i8U&usqp=CAU'
+    },
+    {
+      id: 2,
+      nome: 'Solicitação 2',
+      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpaMt76FseCmxD5YB2nBUxe05lKy6eGYAgsnA09nHUzTQSeFFxaYL5EO_VJuaeYJbNNc0&usqp=CAU'
+    },
+    {
+      id: 3,
+      nome: 'Solicitação 3',
+      imagem: 'https://img.myloview.com.br/fotomurais/vector-futebol-time-futebol-clube-icone-400-114633272.jpg'
+    },
+    {
+      id: 4,
+      nome: 'Solicitação 4',
+      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVJGQztUBhtJD82dfemkDuXpdGsbz_CgUjbP6FsKS0ZWJ9GiMg6aAggeuaTPkIf1cLq7w&usqp=CAU'
+    }
+  ];
 
   constructor() {}
 
+  aprovarSolicitacao(solicitacao: any) {
+    console.log('Solicitação aprovada:', solicitacao.nome);
+    // Lógica para aprovar solicitação
+  }
+
+  rejeitarSolicitacao(solicitacao: any) {
+    console.log('Solicitação rejeitada:', solicitacao.nome);
+    // Lógica para rejeitar solicitação
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -6,13 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
-  constructor() {}
-
+openMyTournaments() {
+throw new Error('Method not implemented.');
 }
 
+  constructor(private navCtrl: NavController) {}
 
+    // Método para voltar à página anterior
+    goBack() {
+      this.navCtrl.back(); // Navega de volta para a página anterior
+    }
 
+  // Método para abrir a página do Calendário
+  openCalendar() {
+    this.navCtrl.navigateForward('/'); // Substitua pelo caminho correto da sua página de calendário
+  }
+}
 
 
 
